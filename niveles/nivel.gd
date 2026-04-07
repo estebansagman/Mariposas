12598476a8a0c23580a488bda7aed96c509ebcb3
estebansagman <sagmanesteban@gmail.com>
2026-04-07 09:23:53 -0300

    Logica de acceso export

--- niveles/nivel.gd
+++ niveles/nivel.gd
@@ -4,11 +4,10 @@ extends Node2D
 var estrellas:int
 var puntos_maximos:int
 
-
 func ir_al_siguiente():
 	var numero_actual = name.to_int() 
 	var siguiente_ruta = "res://niveles/niveles/Nivel_" + str(numero_de_nivel + 1) + ".tscn"
-	if FileAccess.file_exists(siguiente_ruta):
+	if ResourceLoader.exists(siguiente_ruta):
 		get_tree().change_scene_to_file(siguiente_ruta)
 	else:
 		print("¡No hay más niveles!")

a9e89842e1c177be6892c7dbf3c3df0dacd35cb0
estebansagman <sagmanesteban@gmail.com>
2026-04-26 02:34:14 -0300

    Cambio del esquema de datos

--- mariposas/recursos/recurso_mariposa.gd
+++ mariposas/recursos/recurso_mariposa.gd
@@ -1,8 +1,8 @@
 extends Resource
 class_name RecursoMariposa
 
-@export var puntos_que_suma: int
 @export var nombre_Mariposa: String
 @export var textura: Texture2D
 @export var requisitos:Array[Dios.Especie]
-@export var textura_requisitos:Texture2D # esto no va a existir en el futuro, ya que se deberia autosetear
+
+@export var textura_en_libro: Texture2D

3a029ad1e1b17486638f6663ca84cac01621d8b5
HydriaSensus <hydriasensus@gmail.com>
2026-05-24 00:05:37 -0300

    anim_estrella

--- UI/ui.gd
+++ UI/ui.gd
@@ -14,6 +14,7 @@ signal pasar_nivel
 
 @onready var nine_patch_rect: NinePatchRect = $NinePatchRect
 const HIGHLIGHT = preload("uid://dshrtbs2itfh0")
+const ESTRELLA = preload("uid://dpw0savrm3hul")
 
 var superado:bool = false
 
@@ -27,7 +28,7 @@ func apagar_alerta_de_seleccion(): # esto se modifica, alerta reinicio
 func superar_nivel():
 	if !superado:
 		anim_win()
-		cartel_final.show()
+		#cartel_final.show()
 		catalogo_mariposas.animacion_ganar()
 
 func anim_win()->void:
@@ -36,6 +37,7 @@ func anim_win()->void:
 	await get_tree().create_timer(1.4).timeout
 	vfx_light.queue_free()
 	highlight_mariposa()
+	anim_estrella()
 
 func highlight_mariposa()->void:
 	#var jardin = get_tree().current_scene.find_child("Jardin")
@@ -44,6 +46,20 @@ func highlight_mariposa()->void:
 	for mariposa in get_parent().find_children("*","Mariposa",true,false):
 		print("Highilight para ",mariposa)
 		mariposa.agregar_highlight()
+
+func anim_estrella()->void:
+	for mariposa in get_parent().find_children("*","Mariposa",true,false):
+		var estrella:Sprite2D = Sprite2D.new()
+		var t = create_tween()
+		estrella.scale = Vector2(2.0,2.0)
+		estrella.z_index = 6
+		estrella.texture = ESTRELLA
+		get_parent().add_child(estrella,true)
+		estrella.global_position = mariposa.find_child("Sprite2D",true,false).global_position
+		t.set_ease(Tween.EASE_IN)
+		t.set_trans(Tween.TRANS_BACK)
+		t.tween_property(estrella,"global_position",Vector2(960,60),1.0)
+
 
 func ocultar_cartel():
 	cartel_final.hide()

82ee30c4531f8cd23b59c613b9a1c72988cdee4c
bergia00 <bergia00@gmail.com>
2026-06-01 15:16:53 -0300

    Revert "Merge branch 'main' into bergia"
    This reverts commit dc09d0ec9d94d7a0766148057047436cea87ca68, reversing
    changes made to 1a3d497e17c7178d4dfb679ee095a8f9670bd41e.

--- Libro/contenedor_guias.gd
+++
@@ -1,7 +0,0 @@
-extends HBoxContainer
-var nombre:String
-@onready var boton: TextureButton = $AspectRatioContainer/Boton
-@onready var titulo: TextureRect = $AspectRatioContainer/Boton/Control/Titulo
-
-func prueba():
-	boton.texture_normal

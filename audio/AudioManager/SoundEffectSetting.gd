de90e51dcb08838ea4081588b19074466f843021
estebansagman <sagmanesteban@gmail.com>
2026-06-11 02:37:22 -0300

    pa recibir commit

--- audio/AudioManager/SoundEffectSetting.gd
+++ audio/AudioManager/SoundEffectSetting.gd
@@ -36,7 +36,7 @@ enum SOUND_EFFECT_TYPE {
 @export_range(0, 10) var limit: int = 5 ## Maximum number of this SoundEffect to play simultaneously before culled.
 @export var type: SOUND_EFFECT_TYPE ## The unique sound effect in the [enum SOUND_EFFECT_TYPE] to associate with this effect. Each SoundEffect resource should have it's own unique [enum SOUND_EFFECT_TYPE] setting.
 @export var sound_effect: AudioStream ## The [AudioStreamMP3] audio resource to play.
-@export_enum("sfx", "ui","musica") var canal_audio: String = "SFX"
+@export_enum("sfx", "ui","musica") var canal_audio: String = "sfx"
 @export_range(-40, 20) var volume: float = 0 ## The volume of the [member sound_effect].
 @export_range(0.0, 4.0,.01) var pitch_scale: float = 1.0 ## The pitch scale of the [member sound_effect].
 @export_range(0.0, 1.0,.01) var pitch_randomness: float = 0.0 ## The pitch randomness setting of the [member sound_effect].
